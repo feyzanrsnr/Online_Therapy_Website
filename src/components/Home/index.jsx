25754b0af27665037e51fccd2f55a8526d91c741
feyzanursener <senerffeyza@gmail.com>
2025-06-12 16:58:16 +0300

    new

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,7 +4,7 @@ import image from '../../assets/images/ChatGPT Image 12 Haz 2025 03_28_35.png'
 function index() {
   return (
     <div className='section home'>
-        <img src={image} alt="home-image" />
+        {/* <img src={image} alt="home-image" /> */}
 
         {/* <p className='home-text'>“İyi hissetmek, güçlü olmaktan daha değerlidir.”</p> */}
     </div>
