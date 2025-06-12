ab1bf72bddfae3de960d9281417c41c4625f220d
feyzanursener <senerffeyza@gmail.com>
2025-06-12 16:28:22 +0300

    responsive updates

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,7 +7,7 @@ const url = 'https://github.com/feyzanrsnr'
 function index() {
   return (
     <footer className='footer'>
-      <p className='text'>
+      <p>
         Copyright &#169; 2025 |All right received| Designed and Developed by <Link target='blank' to={url} style={{textDecoration:'underline'}}>Feyzanur Sener</Link>
       </p>
       
